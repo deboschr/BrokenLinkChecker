39da1b23129fea09104c9d616f839a30b507d607
deboschr <debospencher@gmail.com>
2025-09-17 09:33:22 +0700

    oke

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,7 +1,9 @@
 module com.unpar.webcrawler {
     requires org.jsoup;
     requires java.net.http;
-
+    requires javafx.controls;
+    requires javafx.fxml;
 
     exports com.unpar.webcrawler;
+    opens com.unpar.webcrawler to javafx.fxml;
 }
